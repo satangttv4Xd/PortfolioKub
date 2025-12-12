a2bef5cfc6741d2193c69fcf7e3dc01640d4682d
satangttv4Xd <satangttv123@gmail.com>
2025-12-12 11:14:02 +0700

    update infinite scroll

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ window.addEventListener('scroll', () => {
         navbar.style.backgroundColor = 'rgba(10, 10, 10, 0.98)' :
         navbar.style.backgroundColor = 'rgba(10, 10, 10, 0.95)';
 });
+
+var texts = [
+    "Full-stack Developer",
+    "Penetration Testing"
+];
+
+var textIndex = 0;
+var charIndex = 0;
+var speed = 100;
+var el = document.getElementById("header-typing");
+
+function typeWriter() {
+    if (charIndex < texts[textIndex].length) {
+        el.innerHTML =
+            texts[textIndex].substring(0, charIndex + 1) +
+            '<span class="cursor">|</span>';
+        charIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(() => {
+            charIndex = 0;
+            textIndex = (textIndex + 1) % texts.length;
+            el.innerHTML = "";
+            typeWriter();
+        }, 1500);
+    }
+}
+
+typeWriter();
+
+
